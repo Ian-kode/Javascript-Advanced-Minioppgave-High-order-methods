434645e565c65b478328bdc761df2629ebbb563c
Ian.kodehode <ian.kodehode@gmail.com>
2025-08-11 14:27:46 +0200

    finished case 2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const products = [
 // CASE #1 ------------------------------------------------------------------
 
 // filter
-const cheapestProduct = products.filter((item) => item.price < 200);
+const cheapestProduct = products.filter((item) => item.price < 200); // Using filter() to create and return a new array with objects that have "price" property in the products array which is less than 200.
 console.log(`Products on sale for 200nok or less:`, cheapestProduct);
 
 // map
@@ -26,9 +26,9 @@ console.log(`Electronic Products in stock: ${electronicProducts}`);
 
 // some, filter, map
 let sellTheseToPoshPeeps = [];
-if (products.some((item) => item.price >= 1000)) {
+if (products.some((item) => item.price > 1000)) {
   sellTheseToPoshPeeps = products
-    .filter((item) => item.price >= 1000)
+    .filter((item) => item.price > 1000)
     .map((item) => item.name);
 } else sellTheseToPoshPeeps = [];
 console.log(`Luxury Products: ${sellTheseToPoshPeeps}`);
@@ -36,7 +36,33 @@ console.log(`Luxury Products: ${sellTheseToPoshPeeps}`);
 // reduce
 // prettier-ignore
 const totalWorthOfProducts = products.reduce(
-  (accumulated, currentValue) => accumulated + currentValue.price, 0);
+  (accumulator, currentValue) => accumulator + currentValue.price, 0);
 console.log(`Total worth of inventory: ${totalWorthOfProducts}`);
 
 // CASE #2------------------------------------------------------------------------
+// prettier-ignore
+const possibleTotalEarnings = products.reduce((accumulator, currentPrice) => accumulator + currentPrice.price, 0)
+console.log(`Total possible earnings: ${possibleTotalEarnings}`);
+
+let productsToSell = [];
+if (products.some((item) => item.price <= 200)) {
+  productsToSell = products
+    .filter((item) => item.price <= 200)
+    .map((item) => item.name);
+  console.log(`Products on SALE!!! ${productsToSell}`);
+} else productsToSell = [];
+
+if (products.some((item) => item.category === "clothing")) {
+  clothingProducts = products
+    .filter((item) => item.category === "clothing")
+    .map((item) => item.name);
+  console.log(`Special discount on clothings! ${clothingProducts}`);
+} else clothingProducts = [];
+
+let luxProductsDiscounted = [];
+if (products.some((item) => item.price > 1000)) {
+  luxProductsDiscounted = products
+    .filter((item) => item.price > 1000)
+    .map((item) => item.name);
+} else luxProductsDiscounted = [];
+console.log(`Special discount on luxury products: ${luxProductsDiscounted}`);
